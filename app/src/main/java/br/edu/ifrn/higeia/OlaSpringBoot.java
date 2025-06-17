0a49a69fad2d8038f248cd8f03fe2238d290cf09
bianca <bianca.nascimento@escolar.ifrn.edu.br>
2025-06-17 16:12:18 -0300

    tabelas atualizadas

--- app/src/main/java/br/edu/ifrn/higeia/OlaSpringBoot.java
+++ app/src/main/java/br/edu/ifrn/higeia/OlaSpringBoot.java
@@ -1,4 +1,4 @@
-package br.edu.ifrn.higeia;;
+package br.edu.ifrn.cafeteria;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
